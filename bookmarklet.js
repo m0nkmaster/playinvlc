84b619adff0176a7f9606992501b5e91e97347d9
m0nkmaster <rob@robmacdonald.com>
2012-12-23 23:43:57 +0000

    Added initial bookmarklet.

---
+++ bookmarklet.js
@@ -0,0 +1,33 @@
+vlcHost = 'http://192.168.1.73:8080';
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+function addScript(src) {
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = src;
+   head.appendChild(script);
+}
+
+anchors = document.querySelectorAll('a');
+
+for (var i=0; i <= anchors.length; i++){
+	if (anchors[i].href.endsWith('hi.mp4')) {
+		addScript(vlcHost + '/requests/status.xml?command=fullscreen');
+		addScript(vlcHost + '/requests/status.xml?command=in_play&input=' + anchors[i]);
+		break;
+	}
+}
+
+
+
+// ----
+// VLC Commands
+// 
+// Playing a file:
+// http://192.168.1.73:8080/requests/status.xml?command=in_play&input=FILENAME
+
+// Full screen:
+// http://192.168.1.73:8080/requests/status.xml?command=fullscreen
