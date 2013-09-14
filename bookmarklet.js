60a254e19a9aaffe0568ce839e124672bcfc57fb
m0nkmaster <rob@robmacdonald.com>
2013-09-14 22:32:19 +0100

    Fixed JSLinting issues and added comment block

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,33 +1,46 @@
-vlcHost = 'http://192.168.1.73:8080';
-String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
+/**
+ * PlayInVLC
+ * 
+ * A short script, intended to be used as a bookmarklet, to send a link present on a given page to a VLC server.
+ * 
+ * VLC basics:
+ * 
+ * Playing a file:
+ * http://192.168.1.73:8080/requests/status.xml?command=in_play&input=FILENAME
+ * 
+ * Full screen:
+ * http://192.168.1.73:8080/requests/status.xml?command=fullscreen
+ * 
+ */
 
-function addScript(src) {
-   var head = document.getElementsByTagName('head')[0];
-   var script = document.createElement('script');
-   script.type = 'text/javascript';
-   script.src = src;
-   head.appendChild(script);
-}
+(function () {
+    "use strict";
 
-anchors = document.querySelectorAll('a');
+    var vlcHost = 'http://192.168.1.73:8080',
+        fileSuffix = 'hi.mp4',
+        anchors = document.querySelectorAll('a'),
+        i = 0;
 
-for (var i=0; i <= anchors.length; i++){
-    if (anchors[i].href.endsWith('hi.mp4')) {
-        if (confirm ("Go fullscreen?")) {
-            addScript(vlcHost + '/requests/status.xml?command=fullscreen');
-        }
-        addScript(vlcHost + '/requests/status.xml?command=in_play&input=' + anchors[i]);
-        break;
+    String.prototype.endsWith = function (suffix) {
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+
+    function addScript(src) {
+        var head = document.getElementsByTagName('head')[0],
+            script = document.createElement('script');
+
+        script.type = 'text/javascript';
+        script.src = src;
+        head.appendChild(script);
     }
-}
 
-// ----
-// VLC Commands
-// 
-// Playing a file:
-// http://192.168.1.73:8080/requests/status.xml?command=in_play&input=FILENAME
-
-// Full screen:
-// http://192.168.1.73:8080/requests/status.xml?command=fullscreen
+    for (i = 0; i <= anchors.length; i += 1) {
+        if (anchors[i].href.endsWith(fileSuffix)) {
+            if (confirm("Toggle fullscreen?")) {
+                addScript(vlcHost + '/requests/status.xml?command=fullscreen');
+            }
+            addScript(vlcHost + '/requests/status.xml?command=in_play&input=' + anchors[i]);
+            break;
+        }
+    }
+});
